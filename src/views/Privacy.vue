d135c0208b592c72e562aea7654ef70538c66f53
Dan Benton <dan@dogsbodytechnology.com>
2019-01-10 16:38:55 +0000

    Small spacing and typo changes

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -8,13 +8,15 @@
 
 <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data.</p>
 
-<p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from https://www.statuspile.com/</p>
+<p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in <a href="/terms">our Terms and Conditions</a>.</p>
 
 <h2>Information Collection And Use</h2>
 
 <p>We collect several different types of information for various purposes to provide and improve our Service to you.</p>
 
 <h3>Types of Data Collected</h3>
+
+<p> </p>
 
 <h4>Personal Data</h4>
 
@@ -35,7 +37,7 @@
 
 <p>We use cookies and similar tracking technologies to track the activity on our Service and hold certain information.</p>
 
-<p>Cookies are files with small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyze our Service.</p>
+<p>Cookies are files with small amounts of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyze our Service.</p>
 
 <p>You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.</p>
 
@@ -69,17 +71,17 @@
 
 <p>Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.</p>
 
-<p>If you are located outside United Kingdom and choose to provide information to us, please note that we transfer the data, including Personal Data, to United Kingdom and process it there.</p>
+<p>If you are located outside the United Kingdom and choose to provide information to us, please note that we transfer the data, including Personal Data, to the United Kingdom and process it there.</p>
 
 <p>Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.</p>
 
-<p>Dogsbody Technology Ltd. will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.</p>
+<p>Dogsbody Technology Ltd. will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organisation or a country unless there are adequate controls in place including the security of your data and other personal information.</p>
 
 <h2>Disclosure Of Data</h2>
 
 <h3>Legal Requirements</h3>
 
-<p>Dogsbody Technology Ltd. may disclose your Personal Data in the good faith belief that such action is necessary to:</p>
+<p>Dogsbody Technology Ltd. may disclose your Personal Data in the good faith belief that such action is necessary:</p>
 
 <ul>
 <li>To comply with a legal obligation</li>
@@ -128,8 +130,6 @@
 <h2>Changes To This Privacy Policy</h2>
 
 <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
-
-<p>We will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective and update the "effective date" at the top of this Privacy Policy.</p>
 
 <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
 
