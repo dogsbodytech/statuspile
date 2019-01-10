bb335cac72dc4e5b495682594faff71ef3c22c61
Dan Benton <dan@dogsbodytechnology.com>
2019-01-10 16:16:04 +0000

    Fix spacing

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -25,6 +25,8 @@
 <li>Cookies and Usage Data</li>
 </ul>
 
+<p> </p>
+
 <h4>Usage Data</h4>
 
 <p>We may also collect information on how the Service is accessed and used ("Usage Data"). This Usage Data may include information such as your computer's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</p>
@@ -45,6 +47,8 @@
 <li><strong>Security Cookies.</strong> We use Security Cookies for security purposes.</li>
 </ul>
 
+<p> </p>
+
 <h2>Use of Data</h2>
     
 <p>Dogsbody Technology Ltd. uses the collected data for various purposes:</p>
@@ -58,6 +62,8 @@
 <li>To monitor the usage of the Service</li>
 <li>To detect, prevent and address technical issues</li>
 </ul>
+
+<p> </p>
 
 <h2>Transfer Of Data</h2>
 
@@ -83,6 +89,8 @@
 <li>To protect against legal liability</li>
 </ul>
 
+<p> </p>
+
 <h2>Security Of Data</h2>
 
 <p>The security of your data is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.</p>
@@ -103,6 +111,8 @@
   </li>
 </ul>
 
+<p> </p>
+
 <h2>Links To Other Sites</h2>
 
 <p>Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
@@ -114,7 +124,6 @@
 <p>Our Service does not address anyone under the age of 18 ("Children").</p>
 
 <p>We do not knowingly collect personally identifiable information from anyone under the age of 18. If you are a parent or guardian and you are aware that your Children has provided us with Personal Data, please contact us. If we become aware that we have collected Personal Data from children without verification of parental consent, we take steps to remove that information from our servers.</p>
-
 
 <h2>Changes To This Privacy Policy</h2>
 
