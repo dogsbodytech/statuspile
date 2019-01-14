1036de85d97e8f83e7e7db7d37f189534ae3e479
Dan Benton <dan@dogsbodytechnology.com>
2019-01-14 16:14:27 +0000

    Change order
    So that we can easily find and add providers in the future

--- src/providers.js
+++ src/providers.js
@@ -5,26 +5,52 @@
  */
 
 export default [
+  /** Cachet providers - https://cachethq.io/ */
+  {
+    name: "DNS Made Easy",
+    type: "cachet",
+    url: "https://dnsstatus.com"
+  },
+  {
+    name: "Dogsbody Technology",
+    type: "cachet",
+    url: "https://status.dogsbody.com"
+  },
+  {
+    name: "Magento Commerce (Cloud)",
+    type: "cachet",
+    url: "https://status.magento.cloud/"
+  },
+  {
+    name: "openSUSE",
+    type: "cachet",
+    url: "https://status.opensuse.org/"
+  },
+  {
+    name: "UK2",
+    type: "cachet",
+    url: "https://status.uk2.net/"
+  },
   /** Statuspage providers - https://www.statuspage.io/ */
+  {
+    name: "Atlassian",
+    type: "statuspage",
+    url: "https://status.atlassian.com"
+  },
   {
     name: "BitBucket",
     type: "statuspage",
     url: "https://status.bitbucket.org"
   },
   {
-    name: "Linode",
-    type: "statuspage",
-    url: "https://status.linode.com"
-  },
-  {
-    name: "New Relic",
-    type: "statuspage",
-    url: "https://status.newrelic.com"
-  },
-  {
     name: "CloudFlare",
     type: "statuspage",
     url: "https://www.cloudflarestatus.com"
+  },
+  {
+    name: "Datadog",
+    type: "statuspage",
+    url: "https://status.datadoghq.com"
   },
   {
     name: "Digital Ocean",
@@ -42,6 +68,16 @@ export default [
     url: "https://status.lastpass.com"
   },
   {
+    name: "Linode",
+    type: "statuspage",
+    url: "https://status.linode.com"
+  },
+  {
+    name: "New Relic",
+    type: "statuspage",
+    url: "https://status.newrelic.com"
+  },
+  {
     name: "NTP Pool",
     type: "statuspage",
     url: "https://status.ntppool.org/"
@@ -50,41 +86,5 @@ export default [
     name: "Reddit",
     type: "statuspage",
     url: "https://reddit.statuspage.io"
-  },
-  {
-    name: "Datadog",
-    type: "statuspage",
-    url: "https://status.datadoghq.com"
-  },
-  {
-    name: "Atlassian",
-    type: "statuspage",
-    url: "https://status.atlassian.com"
-  },
-  /** Cachet providers - https://cachethq.io/ */
-  {
-    name: "Dogsbody Technology",
-    type: "cachet",
-    url: "https://status.dogsbody.com"
-  },
-  {
-    name: "DNS Made Easy",
-    type: "cachet",
-    url: "https://dnsstatus.com"
-  },
-  {
-    name: "openSUSE",
-    type: "cachet",
-    url: "https://status.opensuse.org/"
-  },
-  {
-    name: "UK2",
-    type: "cachet",
-    url: "https://status.uk2.net/"
-  },
-  {
-    name: "Magento Commerce (Cloud)",
-    type: "cachet",
-    url: "https://status.magento.cloud/"
   }
 ];
